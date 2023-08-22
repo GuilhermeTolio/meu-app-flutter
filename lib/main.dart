dff505f30b97b9af57582bebac35d6c8afe6727c
Guilherme Tolio dos Santos <guilhermetolio@gmail.com>
2023-08-22 17:41:17 -0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,50 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
-void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
+void main() {
+  runApp(ColorPageApp());
+}
+
+class ColorPageApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: "Calculator App",
-      theme: new ThemeData(primarySwatch: Colors.red),
-      home: new HomePage(),
+    return MaterialApp(
+      title: 'Color Page',
+      home: ColorPage(),
+    );
+  }
+}
+
+class ColorPage extends StatelessWidget {
+  final List<Color> colors = [
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.yellow,
+    Colors.orange,
+    Colors.purple,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Color Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            for (Color color in colors)
+              Container(
+                width: 100,
+                height: 100,
+                color: color,
+                margin: EdgeInsets.all(10),
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
